5f4eb862242b9d426314233f6a00547b75fe6ea6
Luis Flores <lflores02142@gmail.com>
2020-06-14 15:16:07 -0700

    added homepage divs and text. Also added Sass as a dependency

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import HomePage from './Homepage.component';
+
+import "./App.css";
+
+function App() {
+  
+  
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  
+}
+
+export default App;
