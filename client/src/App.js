527fadbb6d20abf808bfd650bc36deaaa37e45d8
Luis Flores <lflores02142@gmail.com>
2020-06-15 13:32:33 -0700

    added a custom-button component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component"
-import Header from './components/header/header.component'
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInRegister from "./pages/signin-register/signin-register.component";
+
 import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
-
-
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInRegister} />
       </Switch>
     </div>
   );
---
+++ client/src/pages/signin-register/signin-register.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SignIn from "../../components/sign-in/sign-in.component";
+import "./signin-register.styles.scss";
+
+const SignInRegister = () => {
+  return (
+    <div className="signin-register">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInRegister;
