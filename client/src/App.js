e5f8c0f3c8bb845d3cd78fa366f63248e08cb825
Luis Flores <lflores02142@gmail.com>
2020-06-15 19:12:45 -0700

    implemented firebase utils included ability to store authenticated users in our database

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInRegister from "./pages/signin-register/signin-register.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 import { Switch, Route } from "react-router-dom";
 
@@ -19,18 +19,33 @@ class App extends React.Component {
   }
   unsubscribeFromAuth = null;
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+          console.log(this.state)
+        });
+      } else {
+        this.setState({
+          currentUser: userAuth,
+        });
+      }
     });
   }
-componentWillUnmount() {
-  this.unsubscribeFromAuth()
-}
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
--- client/src/pages/signin-register/signin-register.component.jsx
+++ client/src/pages/signin-register/signin-register.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import SignIn from "../../components/sign-in/sign-in.component";
+import Register from "../../components/register/register.component"
 import "./signin-register.styles.scss";
 
 const SignInRegister = () => {
   return (
     <div className="signin-register">
       <SignIn />
+      <Register />
     </div>
   );
 };
