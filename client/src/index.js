eaca7a66eae18d4ed6a35d5b720bd8c2a14549a8
Luis Flores <lflores02142@gmail.com>
2020-06-16 16:36:47 -0700

    added redux and set up userReducer and root-reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App increment={1} />
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <App increment={1} />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
