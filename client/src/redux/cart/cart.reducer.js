72078f852135897afbf9acadfe0cfdeba212207e
Luis Flores <lflores02142@gmail.com>
2020-06-17 22:56:28 -0700

    can now add items to cart but quantity doesn't get updated

--- client/src/redux/cart/cart.reducer.js
+++ client/src/redux/cart/cart.reducer.js
@@ -1,8 +1,8 @@
-
-import CartActionTypes from './cart.types';
+import CartActionTypes from "./cart.types";
 
 const INITIAL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -12,8 +12,14 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         hidden: !state.hidden,
       };
+
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload],
+      };
     default:
       return state;
   }
 };
-export default cartReducer
+export default cartReducer;
